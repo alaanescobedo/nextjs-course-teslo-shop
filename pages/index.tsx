3c98366e23ccf9ade45ae59649417e36419fb16a
Alan Escobedo <alaanprogramacion@gmail.com>
2022-03-20 19:01:41 -0600

    Add the ProductList component on the index page and pass the initialData with type any
    The any type is temporary so as not to break the application, since not all types are compatible between the interface and the data.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import type { NextPage } from 'next'
 import { Typography } from '@mui/material'
 import { ShopLayout } from '../components/layout/ShopLayout'
+import { ProductList } from '../components/products/ProductList'
+import { initialData } from '../database/products'
 
 const Home: NextPage = () => {
   return (
     <ShopLayout title={"Teslo-Shop - Home"} pageDescription={"Find the best products"}>
       <Typography variant="h1">Shop</Typography>
       <Typography variant="h2" sx={{ mb: 1 }}>All products</Typography>
+      <ProductList products={initialData.products as any} />
     </ShopLayout>
   )
 }
