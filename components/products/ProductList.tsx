e49578b1161fb982597370f336a925c5dc48ed18
Alan Escobedo <alaanprogramacion@gmail.com>
2022-03-20 18:55:14 -0600

    Add ProductCard and ProductList components

---
+++ components/products/ProductList.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react"
+
+import { Grid } from "@mui/material"
+
+import { ProductCard } from "./ProductCard"
+import { IProduct } from "../../interfaces"
+
+interface Props {
+  products: IProduct[]
+}
+
+export const ProductList: FC<Props> = ({ products }) => {
+  return (
+    <Grid container spacing={4}>
+      {products.map((product) => (
+        <ProductCard product={product} key={product.slug} />
+      ))}
+    </Grid>
+  )
+}
